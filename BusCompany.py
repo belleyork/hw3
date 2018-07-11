18d85ddae67c30d729aa6678886177e46fa4943b
raquelj6437 <raquelj6437@hstat.org>
2018-07-11 13:54:14 -0400

    finish q2

--- BusCompany.py
+++ BusCompany.py
@@ -1,26 +1,52 @@
-from random import randint
+class Towns:
+    def __init__(self, town1='none', town2='none'):
+        self.town1 = town1
+        self.town2 = town2
 
 
-class Bus:
-    def __init__(self, name, busNum, destination):
-        self.name = name
+class Buses:
+    def busCount(self, busNum=0):
         self.busNum = busNum
-        self.destination = destination
+        return busNum
 
-        destinationChoice1 = 0
-        full = 0
+    def bus1(self, seating=4):
+        self.seating = seating
 
-        num = [1, 2, 3, 4, 5, 6, 7]
-        des = ['a', 'b', 'c', 'd', 'e', 'f']
+        return seating
 
-        for i in range(len(des)):
-            destinationChoice = des[i]
+    def bus2(self, seating=5):
+        self.seating = seating
 
-            full = randint(1, 5)
+        return seating
 
-            if destination == destinationChoice and full != 5:
-                print(name + ' can ride the bus to ' + destination +
-                      ' because there are only ' + str(full) + ' people on it.')
-            elif destination == destinationChoice and full == 5:
-                print(name + ' cannot ride the bus to ' + destination +
-                      ' because the bus is full with 5 people.')
+    def bus3(self, seating=10):
+        self.seating = seating
+
+        return seating
+
+    def bus4(self, seating=9):
+        self.seating = seating
+
+        return seating
+
+    def bus5(self, seating=11):
+        self.seating = seating
+
+        return seating
+
+    def bus6(self, seating=2):
+        self.seating = seating
+
+        return seating
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+class Roads:
+    def __init__(self, road='none'):
+        self.road = road
+
+    def roads(self): return self.road
--- question2.py
+++ question2.py
@@ -1,5 +1,37 @@
-from BusCompany import Bus
+from BusCompany import Buses
+from BusCompany import Person
+from BusCompany import Towns
+from BusCompany import Roads
 
-print(Bus('Raquel', '6', 'a'))
-print(Bus('James', '6', 'a'))
-print(Bus('Joseph', '6', 'f'))
+destination1 = ['Apple Vill', 'Blue Ville', 'Cranberry',
+                'Dog Town', 'EveryoneVill', 'FriendsVille']
+destination2 = ['Georgia', 'Houston', 'Ithica',
+                'Joseph', 'Kentucky', 'Loserville']
+roads = ['Bella RD', 'Jam DR', 'Short RD', 'Tall Ave', 'Schumacaer Ave']
+busPeople = []
+bus = Buses().busCount()
+person = Person('Raquel')
+person1 = Person('Bella')
+content = ''
+people = 0
+
+busPeople.append(person1.name)
+
+for i in range(len(destination1)):
+    a = Towns(destination1[i], destination2[i])
+    bus += 1
+
+    busPeople.append(person.name)
+
+    if len(busPeople) > Buses().bus1():
+        content = ' is full'
+    else:
+        content = ' has a seat'
+
+    for num in range(5):  # this will make a bunch of chairs and put them into a list
+        number = num * num
+        # picks a person from the population
+        people = number % len(busPeople)
+
+    print('bus_' + str(bus) + ' is going to town ' +
+          a.town1 + ' and town ' + a.town2 + content + ' and it is traveling on road ' + roads[int(people)] + '.')

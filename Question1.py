9fa779a1cb55fcc578642cf642dccea6a9cfa46f
raquelj6437 <raquelj6437@hstat.org>
2018-07-13 11:04:20 -0400

    edit files

--- Question1.py
+++ Question1.py
@@ -1,6 +1,6 @@
 from Matrices import Matrices
 
 d = Matrices([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [
-             [1, 2, 3], [4, 5, 6], [7, 8, 9]], 'multiply')
+             [1, 2, 3], [4, 5, 6], [7, 8, 9]], 'multiply')  # creates variable of function
 
 print(d)
--- question2.py
+++ question2.py
@@ -4,9 +4,9 @@ from BusCompany import Towns
 from BusCompany import Roads
 
 destination1 = ['Apple Vill', 'Blue Ville', 'Cranberry',
-                'Dog Town', 'EveryoneVill', 'FriendsVille']  # list of destinations
+                'Dog Town', 'EveryoneVill', 'FriendsVille']
 destination2 = ['Georgia', 'Houston', 'Ithica',
-                'Joseph', 'Kentucky', 'Loserville']  # list of destinations
+                'Joseph', 'Kentucky', 'Loserville']
 roads = ['Bella RD', 'Jam DR', 'Short RD', 'Tall Ave', 'Schumacaer Ave']
 busPeople = []
 bus = Buses().busCount()
@@ -16,22 +16,28 @@ content = ''
 people = 0
 
 busPeople.append(person1.name)
-
 for i in range(len(destination1)):
     a = Towns(destination1[i], destination2[i])
     bus += 1
 
+    newBus = 'Buses().' + 'bus' + str(bus) + '()'
+    newBus = eval(newBus)
+
     busPeople.append(person.name)
 
-    if len(busPeople) > Buses().bus1():
+    if len(busPeople) > newBus:
         content = ' is full'
     else:
         content = ' has a seat'
 
-    for num in range(5):  # this will make a bunch of chairs and put them into a list
+    for num in range(10):  # this will make a bunch of chairs and put them into a list
         number = num * num
         # picks a person from the population
         people = number % len(busPeople)
 
+    for p in range(len(busPeople)):
+        if people > len(roads) - 1:
+            people = int(people / 9)
+
     print('bus_' + str(bus) + ' is going to town ' +
-          a.town1 + ' and town ' + a.town2 + content + ' and it is traveling on road ' + roads[int(people)] + '.')
+          a.town1 + ' and town ' + a.town2 + content + ' and it is traveling on ' + roads[int(people)] + '.')
